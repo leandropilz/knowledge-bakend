f165a36b1545aaceebb417570fb5654e11b96acb
pilz <leandro.pizz@gmail.com>
2019-06-25 09:47:32 -0300

    CAP.353 API de Artigo 01

--- config/routes.js
+++ config/routes.js
@@ -1,22 +1,31 @@
 module.exports = app => {
     app.route('/users')
-        .post(app.api.user.save)
         .get(app.api.user.get)
+        .post(app.api.user.save)
 
     app.route('/users/:id')
-        .put(app.api.user.save)
         .get(app.api.user.getById)
+        .put(app.api.user.save)
 
     app.route('/categories')
-        .post(app.api.category.save)
         .get(app.api.category.get)
+        .post(app.api.category.save)
 
     //Cuidado com ordem, tem que vir antes de /categories/:id
     app.route('/categories/tree')
         .get(app.api.category.getTree)
 
     app.route('/categories/:id')
-        .put(app.api.category.save)
         .get(app.api.category.getById)
+        .put(app.api.category.save)
         .delete(app.api.category.remove)
+
+    app.route('/articles')
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+
+    app.route('/articles/:id')
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove)
 }
