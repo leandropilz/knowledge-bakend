81295a3a0a0d926a6d23a2ef712dd77f5bfdcaa0
pilz <leandro.pizz@gmail.com>
2019-06-25 17:11:19 -0300

    CAP.361 Informacoes de conexao no .env

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,8 @@
+const { db } = require('./.env')
+
 module.exports = {
     client: 'postgresql',
-    connection: {
-        database: 'knowledge',
-        user: 'postgres',
-        password: '@Ivb8919'
-    },
+    connection: db,
     pool: {
         min: 2,
         max: 10
