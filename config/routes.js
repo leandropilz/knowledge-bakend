020731fae61d8d02873103d711100b41dac4ebf6
pilz <leandro.pizz@gmail.com>
2019-06-25 14:03:15 -0300

    CAP.355 API de autenticaçao

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 module.exports = app => {
+    app.post('/signup', app.api.user.save)
+    app.post('/signin', app.api.auth.signin)
+    app.post('/validateToken', app.api.auth.validateToken)
+
     app.route('/users')
         .get(app.api.user.get)
         .post(app.api.user.save)
@@ -31,4 +35,5 @@ module.exports = app => {
 
     app.route('/categories/:id/articles')
         .get(app.api.article.getByCategory)
+
 }
